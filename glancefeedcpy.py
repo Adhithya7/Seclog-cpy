681803d9ce34ad2ec1281c92fe9af836b180810d
Athithiyan Packirisamy <38016187+Adhithya7@users.noreply.github.com>
2019-03-21 12:40:30 +0530

    Update glancefeedcpy.py

--- glancefeedcpy.py
+++ glancefeedcpy.py
@@ -50,7 +50,7 @@ for i in range(1,len(hostl)):
     
     for logfile in glanlog:
         s= requests.Session()
-        link = 'https://192.168.0.200:8080/glance'
+        link = 'http://192.168.0.200:8080/glance'
         r = s.get(link,host,logfile)
         g = r.text
         fh = open('/seclog/inter.txt','w+')
