39f2f9579d34071f800fd79fd5b79d949b58fc13
Athithiyan Packirisamy <38016187+Adhithya7@users.noreply.github.com>
2019-03-22 16:06:10 +0530

    Update neutronfeedcpy.py

--- neutronfeedcpy.py
+++ neutronfeedcpy.py
@@ -52,7 +52,7 @@ for i in range(1,len(hostl)):
     
     for logfile in neulog:
         s= requests.Session()
-        link = 'http://192.168.0.200:8080/neutron'
+        link = 'http://192.168.0.200:8080/'
         r = s.get(link+'?host_name='+host+'&log_namein='+logfile)
         g = r.text
         fh = open('/seclog/inter.txt','w+')
